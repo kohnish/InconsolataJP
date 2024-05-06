888357698311f7ca3b612ae4f65c8c0bca31e8a1
kohnish <kohnish@gmx.com>
2024-05-06 05:03:30 +0200

    os2width script rename failing

--- scripts/os2width_reviser_shindim.sh
+++ scripts/os2width_reviser_shindim.sh
@@ -1,16 +1,19 @@
-#!/bin/sh
+#!/bin/bash
+
+set -e
+
 ttx -t OS/2 InconsolataJP*.ttf
 find InconsolataJP*.ttx | xargs sed -i "s/xAvgCharWidth value=\".../xAvgCharWidth value=\"500/g"
 # for Mac
 # find RictyShinShinDiminished*.ttx | xargs sed -i .bak "s/xAvgCharWidth value=\".../xAvgCharWidth value=\"500/g"
-rename "s/ttf/ttb/;" InconsolataJP*.ttf
+
+for file in InconsolataJP*.ttf; do
+    mv -- "$file" "${file%.ttf}.ttb"
+done
+
 ttx -m InconsolataJP-Bold.ttb InconsolataJP-Bold.ttx
 ttx -m InconsolataJP-BoldOblique.ttb InconsolataJP-BoldOblique.ttx
 ttx -m InconsolataJP-Oblique.ttb InconsolataJP-Oblique.ttx
 ttx -m InconsolataJP-Regular.ttb InconsolataJP-Regular.ttx
-ttx -m InconsolataJPDiscord-Bold.ttb InconsolataJPDiscord-Bold.ttx
-ttx -m InconsolataJPDiscord-BoldOblique.ttb InconsolataJPDiscord-BoldOblique.ttx
-ttx -m InconsolataJPDiscord-Oblique.ttb InconsolataJPDiscord-Oblique.ttx
-ttx -m InconsolataJPDiscord-Regular.ttb InconsolataJPDiscord-Regular.ttx
 rm -f *.ttb
 rm -f *.ttx
