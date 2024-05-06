ace2ec46d5a4ae83d24bdaeffd767bbcf80b2596
kohnish <kohnish@gmx.com>
2024-05-06 04:37:29 +0200

    os2width script wrong path

--- scripts/os2width_reviser_shindim.sh
+++ scripts/os2width_reviser_shindim.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-ttx -t OS/2 RictyShinDiminished*.ttf
-find RictyShinDiminished*.ttx | xargs sed -i "s/xAvgCharWidth value=\".../xAvgCharWidth value=\"500/g"
+ttx -t OS/2 InconsolataJP*.ttf
+find InconsolataJP*.ttx | xargs sed -i "s/xAvgCharWidth value=\".../xAvgCharWidth value=\"500/g"
 # for Mac
 # find RictyShinShinDiminished*.ttx | xargs sed -i .bak "s/xAvgCharWidth value=\".../xAvgCharWidth value=\"500/g"
-rename "s/ttf/ttb/;" RictyShinDiminished*.ttf
-ttx -m RictyShinDiminished-Bold.ttb RictyShinDiminished-Bold.ttx
-ttx -m RictyShinDiminished-BoldOblique.ttb RictyShinDiminished-BoldOblique.ttx
-ttx -m RictyShinDiminished-Oblique.ttb RictyShinDiminished-Oblique.ttx
-ttx -m RictyShinDiminished-Regular.ttb RictyShinDiminished-Regular.ttx
-ttx -m RictyShinDiminishedDiscord-Bold.ttb RictyShinDiminishedDiscord-Bold.ttx
-ttx -m RictyShinDiminishedDiscord-BoldOblique.ttb RictyShinDiminishedDiscord-BoldOblique.ttx
-ttx -m RictyShinDiminishedDiscord-Oblique.ttb RictyShinDiminishedDiscord-Oblique.ttx
-ttx -m RictyShinDiminishedDiscord-Regular.ttb RictyShinDiminishedDiscord-Regular.ttx
+rename "s/ttf/ttb/;" InconsolataJP*.ttf
+ttx -m InconsolataJP-Bold.ttb InconsolataJP-Bold.ttx
+ttx -m InconsolataJP-BoldOblique.ttb InconsolataJP-BoldOblique.ttx
+ttx -m InconsolataJP-Oblique.ttb InconsolataJP-Oblique.ttx
+ttx -m InconsolataJP-Regular.ttb InconsolataJP-Regular.ttx
+ttx -m InconsolataJPDiscord-Bold.ttb InconsolataJPDiscord-Bold.ttx
+ttx -m InconsolataJPDiscord-BoldOblique.ttb InconsolataJPDiscord-BoldOblique.ttx
+ttx -m InconsolataJPDiscord-Oblique.ttb InconsolataJPDiscord-Oblique.ttx
+ttx -m InconsolataJPDiscord-Regular.ttb InconsolataJPDiscord-Regular.ttx
 rm -f *.ttb
 rm -f *.ttx
