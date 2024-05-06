686efe54101ff17dda81f9a112b240a19203fba2
kohnish <kohnish@gmx.com>
2024-05-06 03:50:31 +0200

    Make third party dep zip

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,7 +7,7 @@ curl -L -O "https://github.com/googlefonts/Inconsolata/blob/v3.000/fonts/ttf/Inc
 curl -L -O "https://github.com/itouhiro/mixfont-mplus-ipa/releases/download/v2020.0415/circle-mplus-1m-20200415.7z"
 curl -L -O "https://ftp.iij.ad.jp/pub/osdn.jp/users/8/8597/mgenplus-20150602.7z"
 
-rm -rf ./mplus-TESTFLIGHT-* ./circle-mplus-1m-*/ *.ttf *.txt fonts
+rm -rf ./mplus-TESTFLIGHT-* ./circle-mplus-1m-*/ *.ttf *.txt fonts/ thirdparty-dependency-backup/ InconsolataJP/
 
 7z x fonts_ttf.zip
 7z x circle-mplus-1m-20200415.7z
@@ -25,6 +25,7 @@ mkdir -p thirdparty-dependency-backup
 mv fonts_ttf.zip thirdparty-dependency-backup/
 mv circle-mplus-1m-20200415.7z thirdparty-dependency-backup/
 mv mgenplus-20150602.7z thirdparty-dependency-backup
+zip -r thirdparty-dependency-backup.zip thirdparty-dependency-backup
 
 mkdir -p InconsolataJP
 mv InconsolataJP*.ttf InconsolataJP/
