dbd9ab29ccff03719dbfdc8cb76b44ef4970c28f
kohnish <kohnish@gmx.com>
2024-05-06 04:02:43 +0200

    Remove unnecessary download

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,6 @@
 set -e
 
 curl -L -O "https://github.com/googlefonts/Inconsolata/releases/download/v3.000/fonts_ttf.zip"
-curl -L -O "https://github.com/googlefonts/Inconsolata/blob/v3.000/fonts/ttf/Inconsolata-Bold.ttf"
 curl -L -O "https://github.com/itouhiro/mixfont-mplus-ipa/releases/download/v2020.0415/circle-mplus-1m-20200415.7z"
 curl -L -O "https://ftp.iij.ad.jp/pub/osdn.jp/users/8/8597/mgenplus-20150602.7z"
 
