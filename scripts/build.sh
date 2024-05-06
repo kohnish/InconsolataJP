bcefb0602ca45106d0a6ad2bb13664682d346d45
kohnish <kohnish@gmx.com>
2024-05-06 03:10:50 +0200

    Attempt to build in github

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 
+set -e
+
 curl -L -O "https://github.com/googlefonts/Inconsolata/releases/download/v3.000/fonts_ttf.zip"
 curl -L -O "https://github.com/googlefonts/Inconsolata/blob/v3.000/fonts/ttf/Inconsolata-Bold.ttf"
 curl -L -O "https://github.com/itouhiro/mixfont-mplus-ipa/releases/download/v2020.0415/circle-mplus-1m-20200415.7z"
 curl -L -O "https://ftp.iij.ad.jp/pub/osdn.jp/users/8/8597/mgenplus-20150602.7z"
 
-rm -rf ./mplus-TESTFLIGHT-* ./circle-mplus-1m-* *.ttf *.txt
+rm -rf ./mplus-TESTFLIGHT-* ./circle-mplus-1m-*/ *.ttf *.txt fonts
 
-unzip fonts_ttf.zip
+7z x fonts_ttf.zip
 7z x circle-mplus-1m-20200415.7z
 7z x mgenplus-20150602.7z
 
+mv fonts/ttf/* ./
 mv circle-mplus-1m-20200415/* ./
+
+./ricty_shindim_generator.sh auto
+
+# Not working yet
+# ./os2width_reviser_shindim.sh
+
+mkdir -p thirdparty-dependency-backup
+mv fonts_ttf.zip thirdparty-dependency-backup/
+mv circle-mplus-1m-20200415.7z thirdparty-dependency-backup/
+mv mgenplus-20150602.7z thirdparty-dependency-backup
+
+mkdir -p InconsolataJP
+mv InconsolataJP*.ttf InconsolataJP/
+zip -r InconsolataJP.zip InconsolataJP
