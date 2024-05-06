69ed3d79ea9e987127a2274f2ef5b6fd1799718d
kohnish <kohnish@gmx.com>
2024-05-06 02:33:17 +0200

    Make build.sh work on Fedora Linux

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-curl -L -O "https://github.com/googlefonts/Inconsolata/blob/v3.000/fonts/ttf/Inconsolata-Regular.ttf"
+curl -L -O "https://github.com/googlefonts/Inconsolata/releases/download/v3.000/fonts_ttf.zip"
 curl -L -O "https://github.com/googlefonts/Inconsolata/blob/v3.000/fonts/ttf/Inconsolata-Bold.ttf"
 curl -L -O "https://github.com/itouhiro/mixfont-mplus-ipa/releases/download/v2020.0415/circle-mplus-1m-20200415.7z"
 curl -L -O "https://ftp.iij.ad.jp/pub/osdn.jp/users/8/8597/mgenplus-20150602.7z"
-rm -rf ./mplus-TESTFLIGHT-063a/ ./circle-mplus-1m-20200415/
+
+rm -rf ./mplus-TESTFLIGHT-* ./circle-mplus-1m-* *.ttf *.txt
+
+unzip fonts_ttf.zip
 7z x circle-mplus-1m-20200415.7z
+7z x mgenplus-20150602.7z
+
 mv circle-mplus-1m-20200415/* ./
